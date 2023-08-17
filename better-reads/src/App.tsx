d0be02dc0e65d20bf591c43607b4f0fc502aabaf
ryan-g13 <ryan.groesch@gmail.com>
2023-08-17 15:55:34 -0700

    Added todo list

--- better-reads/src/App.tsx
+++ better-reads/src/App.tsx
@@ -30,3 +30,29 @@ function App() {
 }
 
 export default App;
+
+/* 
+TODO: Carousel updates:
+      a. rotation of cards
+      b. update cards styling
+      c. clickable direction underneath
+      d. fetching actual top5 data
+
+      Search:
+      a. wire up button / enter submission
+      b. genre links as filters ? 
+      c. styling
+
+      Header: 
+      a. styling 
+      b. font selection 
+      c. navigation ? or as new comp
+
+      Scrollable list:
+      a. reuse card component from carousel to list books upon search results
+      b. filterable search
+    
+      User / Save to list functionality.
+
+      Clean up original application code
+*/
--- better-reads/src/Carousel.tsx
+++ better-reads/src/Carousel.tsx
@@ -10,7 +10,7 @@ const Carousel = () => {
     const top5 = top5Data.map(ele => {
         return <div className='carousel-card'>
             <img src={ele.img} className='carousel-img'/>
-            <div>{ele.title}</div>
+            <div><b>{ele.title}</b></div>
             <div>{ele.author}</div>
         </div>
     })
@@ -22,4 +22,20 @@ const Carousel = () => {
     )
 }
 
-export default Carousel
+export default Carousel;
+
+// .card {
+//     /* Add shadows to create the "card" effect */
+//     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+//     transition: 0.3s;
+//   }
+  
+//   /* On mouse-over, add a deeper shadow */
+//   .card:hover {
+//     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+//   }
+  
+//   /* Add some padding inside the card container */
+//   .container {
+//     padding: 2px 16px;
+//   }
