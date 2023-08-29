f35b3a4d6a9e81c44774159d9d7332c10fe3f4aa
ryan-g13 <ryan.groesch@gmail.com>
2023-08-29 13:51:33 -0700

    Added Detail view pages and Stats page

---
+++ better-reads/src/DetailView.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DetailView = () => {
+    return (
+        <div className='detail-view-container'>
+            Detailed - Book view
+        </div>
+    )
+}
+
+export default DetailView;
+
+/* 
+TODO: 
+1. Construct interface for a detailed view  
+2. Image, title, author, description - long form, like, add to list/ remove from list
+3. Design page to fit with nav
+*/
